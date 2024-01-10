77565957a301e04a13138ccaba4964f606063ac1
KassCrypto <kasscrypto@gmail.com>
2024-01-10 14:00:38 +0000

    Created  mock oracle for testing

---
+++ contracts/mock/MockDAOracle.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+import "blobstream-contracts/src/IDAOracle.sol";
+
+contract MockDAOracle is IDAOracle {
+    bool public _result;
+
+    function verifyAttestation(
+        uint256 _tupleRootNonce,
+        DataRootTuple memory _tuple,
+        BinaryMerkleProof memory _proof
+    ) external view returns (bool) {
+        return _result;
+    }
+
+    function setResult(bool result) external {
+        _result = result;
+    }
+}
