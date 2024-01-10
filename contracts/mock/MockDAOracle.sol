7f6620cf8476268220107ba8b5c32dc76a951044
KassCrypto <kasscrypto@gmail.com>
2024-01-10 14:58:04 +0000

    configurable _result variable in MockDAOracle.sol

--- contracts/mock/MockDAOracle.sol
+++ contracts/mock/MockDAOracle.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "blobstream-contracts/src/IDAOracle.sol";
 
 contract MockDAOracle is IDAOracle {
-    bool public _result;
+    bool public _result = true;
 
     function verifyAttestation(
         uint256 _tupleRootNonce,
