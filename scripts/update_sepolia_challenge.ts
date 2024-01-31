d2a1679f5da8e85db40bfe27ce44b614a16f5f3f
Sledro <dan@sledro.com>
2024-01-31 22:59:25 +0000

    fix and tidy update proxy script

--- scripts/update_sepolia_challenge.ts
+++ scripts/update_sepolia_challenge.ts
@@ -1,13 +1,8 @@
 import { ethers, network } from "hardhat";
 import { verify } from "../utils/verify";
-import { Contract } from "ethers";
 
 // Set current addresses
-const canonicalStateChainAddr = "0x2A66396cd5D8a29B1b7442596060FB34E4d14989";
-const challengeContractProxyAddr = "0x6C757cE003377975DE0B06247c257e1Fdef2158C";
-const challengeImplAddr = "0x79ce810967eef9c43ac21b7431fdbd10c134cee5";
-const treasuryAddr = "0x3a5cbB6EF4756DA0b3f6DAE7aB6430fD8c46d247";
-const DAOracleAddr = "0xec30E779dcB18A4d2C6039a84b060F66d689fb23";
+const challengeContractProxyAddr = "CONTRACT_ADDRESS";
 
 const main = async () => {
   // Log network name and chain id selected for deployment
@@ -41,44 +36,14 @@ const main = async () => {
   );
 
   // Update Challenge contract proxy to new implementation
-
-  const data = challengeImplementation.interface.encodeFunctionData(
-    "initialize",
-    [treasuryAddr, canonicalStateChainAddr, DAOracleAddr, ethers.ZeroAddress],
-    ownerAddr
-  );
-
   console.log("Updating Challenge proxy to new implementation...");
-  console.log("→ → Calling Challenge.upgradeToAndCall() with data:", data);
-
   const challengeProxy = await ethers.getContractAt(
     "Challenge",
     challengeContractProxyAddr
   );
-  await challengeProxy.upgradeToAndCall(challengeImplementationAddr, data);
+  await challengeProxy.upgradeToAndCall(challengeImplementationAddr, "0x");
   console.log(
-    `→ Challenge proxy updated to ${challengeImplementationAddr}` + "\n"
-  );
-
-  ///
-  /// Set contract setters
-  ///
-
-  // set Challenge.setDefender() to publisherAddr
-  await challengeProxy.setDefender(publisherAddr);
-  console.log(`→ → Challenge.setDefender() set to ${publisherAddr}`);
-
-  // get currently deployed CanonicalStateChain contract from address
-  const canonicalStateChain = await ethers.getContractAt(
-    "CanonicalStateChain",
-    canonicalStateChainAddr
-  );
-
-  // set CanonicalStateChain.challengeContract() to challengeContractAddr
-  await canonicalStateChain.setChallengeContract(challengeImplementationAddr);
-  console.log(
-    `→ → CanonicalStateChain.challengeContract() set to ${challengeImplementationAddr}` +
-      "\n"
+    `→ Challenge proxy (${challengeContractProxyAddr}) updated to ${challengeImplementationAddr}` + "\n"
   );
 
   console.log("All Contracts deployed successfully! \n");
