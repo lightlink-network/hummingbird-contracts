8619177f007c330aa0ad284edc7202b605ff0ffb
KassCrypto <kasscrypto@gmail.com>
2024-04-25 14:27:28 +0100

    add shareroot field to csc

--- contracts/CanonicalStateChain.sol
+++ contracts/CanonicalStateChain.sol
@@ -29,6 +29,7 @@ contract CanonicalStateChain is UUPSUpgradeable, OwnableUpgradeable {
         uint64 l2Height;
         bytes32 prevHash;
         bytes32 stateRoot;
+        bytes32 shareRoot;
         CelestiaPointer[] celestiaPointers;
     }
 
