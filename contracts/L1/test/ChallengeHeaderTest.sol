8fb87417fbc291b7fcd46588dfe94c468cd96b99
CryptoKass <kasscrypto@gmail.com>
2024-06-17 14:10:54 +0100

    chore: move contracts to L1 sub directory

--- contracts/L1/test/ChallengeHeaderTest.sol
+++ contracts/L1/test/ChallengeHeaderTest.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../../contracts/challenge/ChallengeHeader.sol";
+import "../challenge/ChallengeHeader.sol";
 
 // Expose internal functions for testing
 
