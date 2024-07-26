b8eb0c8c1ba1f3f9ee7c53b6bc376ad39ab6fee3
CryptoKass <kasscrypto@gmail.com>
2024-07-26 13:11:44 +0100

    update deploy script to only deploy l2 contracts.

--- scripts/hardhat/deploy/deployBridge.ts
+++ scripts/hardhat/deploy/deployBridge.ts
@@ -11,68 +11,29 @@ import { proxyDeployAndInitialize } from "../lib/deploy";
 // npx hardhat verify --contract contracts/L2/L2CrossDomainMessenger.sol:L2CrossDomainMessenger --network pegasus 0x719E6614628552CFE0CE83B61778b2eA8A5c2147
 // npx hardhat verify --contract contracts/L2/L1Block.sol:L1Block --network pegasus 0x719E6614628552CFE0CE83B61778b2eA8A5c2147
 
-const CanonicalStateChain = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const Challenge = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
+const CanonicalStateChain =
+  process.env.CANONICAL_STATE_CHAIN_ADDR ?? ethers.ZeroAddress;
+const Challenge = process.env.CHALLENGE_ADDR ?? ethers.ZeroAddress;
+const HARDHAT_CHAIN_ID = 31337n;
 
 const main = async () => {
-  const l1Provider = new ethers.JsonRpcProvider(process.env.L1_RPC_URL!);
-  const l1Deployer = new ethers.Wallet(
-    process.env.L1_DEPLOYER_KEY!,
-    l1Provider,
-  );
+  const l1Provider = ethers.provider;
+  const l1Deployer = (await ethers.getSigners())[0];
 
-  const l2Provider = new ethers.JsonRpcProvider(process.env.L2_RPC_URL!);
-  const l2Deployer = new ethers.Wallet(
-    process.env.L2_DEPLOYER_KEY!,
-    l2Provider,
-  );
-
-  // Deploy L2ToL1MessagePasser contract to L2
-  log("Deploying L2ToL1MessagePasser to L2...");
-  const L2ToL1MessagePasser = await ethers.getContractFactory(
-    "L2ToL1MessagePasser",
-    l2Deployer,
-  );
-  const l2ToL1MessagePasser = await L2ToL1MessagePasser.deploy();
-  await l2ToL1MessagePasser.waitForDeployment();
-
-  // Deploy L1Block contract to L2
-  log("Deploying L1Block to L2...");
-  const L1Block = await ethers.getContractFactory("L1Block", l2Deployer);
-  const l1Block = await L1Block.deploy();
-  await l1Block.waitForDeployment();
+  // Check CSC and Chalenge contracts exists
+  if ((await l1Provider.getNetwork()).chainId != HARDHAT_CHAIN_ID) {
+    if (!(await l1Provider.getCode(CanonicalStateChain)).length)
+      throw new Error("CanonicalStateChain contract not deployed");
+    if (!(await l1Provider.getCode(Challenge)).length)
+      throw new Error("Challenge contract not deployed");
+  }
 
   // Deploy LightLinkPortal contract to L1
   log("Deploying LightLinkPortal to L1...");
   const lightLinkPortal = await proxyDeployAndInitialize(
     l1Deployer,
     await ethers.getContractFactory("LightLinkPortal"),
-    [CanonicalStateChain, Challenge, await l1Block.getAddress()],
-  );
-
-  // Cross domain messengers
-  // - Infer deployment addresses before deploying
-  const l2CrossDomainMessengerAddr = ethers.getCreateAddress({
-    from: l2Deployer.address,
-    nonce: (await l2Provider.getTransactionCount(l2Deployer.address)) + 1,
-    // +1 because implementation will be deployed first
-  });
-  const l1CrossDomainMessengerAddr = ethers.getCreateAddress({
-    from: l1Deployer.address,
-    nonce: (await l1Provider.getTransactionCount(l1Deployer.address)) + 1,
-    // +1 because implementation will be deployed first
-  });
-
-  // Deploy L2CrossDomainMessenger contract to L2
-  log("Deploying L2CrossDomainMessenger to L2...");
-  const L2CrossDomainMessengerDeployment = await proxyDeployAndInitialize(
-    l2Deployer,
-    await ethers.getContractFactory("L2CrossDomainMessenger"),
-    [
-      l1CrossDomainMessengerAddr,
-      await l2ToL1MessagePasser.getAddress(),
-      await l1Block.getAddress(),
-    ],
+    [CanonicalStateChain, Challenge],
   );
 
   // Deploy L1CrossDomainMessenger contract to L1
@@ -80,67 +41,30 @@ const main = async () => {
   const L1CrossDomainMessengerDeployment = await proxyDeployAndInitialize(
     l1Deployer,
     await ethers.getContractFactory("L1CrossDomainMessenger"),
-    [lightLinkPortal.address, l2CrossDomainMessengerAddr],
+    [lightLinkPortal.address],
   );
-
-  // Standard bridges
-  // - Infer deployment addresses before deploying
-  const l2StandardBridgeAddr = ethers.getCreateAddress({
-    from: l2Deployer.address,
-    nonce: (await l2Provider.getTransactionCount(l2Deployer.address)) + 1,
-    // +1 because implementations will be deployed first
-  });
-  const l1StandardBridgeAddr = ethers.getCreateAddress({
-    from: l1Deployer.address,
-    nonce: (await l1Provider.getTransactionCount(l1Deployer.address)) + 1,
-    // +1 because implementations will be deployed first
-  });
-
-  // Deploy L2StandardBridge contract to L2
-  log("Deploying L2StandardBridge to L2...");
-  const L2StandardBridgeDeployment = await proxyDeployAndInitialize(
-    l2Deployer,
-    await ethers.getContractFactory("L2StandardBridge"),
-    [
-      l1StandardBridgeAddr,
-      l2CrossDomainMessengerAddr,
-      ethers.ZeroAddress,
-      await l1Block.getAddress(),
-    ],
-  );
+  const l1CrossDomainMessengerAddr = L1CrossDomainMessengerDeployment.address;
 
   // Deploy L1StandardBridge contract to L1
   log("Deploying L1StandardBridge to L1...");
   const L1StandardBridgeDeployment = await proxyDeployAndInitialize(
     l1Deployer,
     await ethers.getContractFactory("L1StandardBridge"),
-    [l1CrossDomainMessengerAddr, l2StandardBridgeAddr],
+    [l1CrossDomainMessengerAddr],
   );
 
   log("Deployment complete!");
 
   // Log deployment addresses
-  log(" L2ToL1MessagePasser:", `"${await l2ToL1MessagePasser.getAddress()}"`);
-  log(" L1Block:", `"${await l1Block.getAddress()}"`);
   log(
     " LightLinkPortal:",
     `"${lightLinkPortal.address}"`,
     `"(impl ${lightLinkPortal.implementationAddress})"`,
   );
   log(
-    " L2CrossDomainMessenger:",
-    `"${L2CrossDomainMessengerDeployment.address}"`,
-    `"(impl ${L2CrossDomainMessengerDeployment.implementationAddress})"`,
-  );
-  log(
     " L1CrossDomainMessenger:",
     `"${L1CrossDomainMessengerDeployment.address}"`,
     `"(impl ${L1CrossDomainMessengerDeployment.implementationAddress})"`,
-  );
-  log(
-    " L2StandardBridge:",
-    `"${L2StandardBridgeDeployment.address}"`,
-    `"(impl ${L2StandardBridgeDeployment.implementationAddress})"`,
   );
   log(
     " L1StandardBridge:",
